b78d88cdb8a60f6a8aea40e8ddcef5f5f9c65bae
Johhny Hasse <johhny@labcloud.app>
2019-09-18 10:27:12 -0300

    correções eslint

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,28 +6,23 @@
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
+      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
         <span class="mr-2">Latest Release</span>
         <v-icon>open_in_new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <router-view/>
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data: () => ({
     //
-  }),
-};
+  })
+}
 </script>
